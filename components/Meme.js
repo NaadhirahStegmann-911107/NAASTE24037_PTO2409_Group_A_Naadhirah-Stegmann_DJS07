1e14d3e147be4f88a0f392e6efba95ae8be579b7
Naadhirah Stegmann <naadhirahsteg@gmail.com>
2025-06-06 20:21:57 +0200

    Accessibility to Text boxes
    Added the labels and correct placeholder texts in order for Screen-reader to describe them correctly.

---
+++ components/Meme.js
@@ -0,0 +1,29 @@
+import React from "react";
+
+export default function Meme() {
+    return (
+        <main>
+            <form className="form">
+                <div>
+                    <label htmlFor="top-text">Top Text</label>
+                    <input 
+                        id="top-text"
+                        type="text"
+                        placeholder="Shut up"
+                        className="form--input"
+                    />
+                </div>
+                <div>
+                    <label htmlFor="bottom-text">Bottom Text</label>
+                    <input
+                        id="bottom-text" 
+                        type="text"
+                        placeholder="and take my money"
+                        className="form--imput"
+                    />
+                </div>
+                <button className="form--button">Get a new meme image🖼️</button>
+            </form>
+        </main>
+    )
+}
