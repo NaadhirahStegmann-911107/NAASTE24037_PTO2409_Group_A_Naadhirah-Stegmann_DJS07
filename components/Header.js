c3e3fb50993e9a1d738d5ff1846f618f59f1a498
Naadhirah Stegmann <naadhirahsteg@gmail.com>
2025-06-06 20:05:23 +0200

    Components
    Based of the scrimba, I created a components folder with a `Header.js` and `Meme.js` files in order to start creating the meme generator layout.
    
    Also added the troll-face.png as well into an image file.

---
+++ components/Header.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export default function Header() {
+    return (
+        <header>
+            <img src="./images/Troll Face PNG.png"/>
+            <h2>Header component</h2>
+            <h4>React Course Project</h4>
+        </header>
+    )
+}
