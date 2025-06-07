6a0986ba5ca6aa54ec7109642c780c39bd625a73
Naadhirah Stegmann <naadhirahsteg@gmail.com>
2025-06-07 21:13:56 +0200

    Images to Meme Generator
    Added images to Meme generator by means of calling on the state as well as creating a `memesData.js` file in order to call upon the information.

--- components/Meme.js
+++ components/Meme.js
@@ -1,29 +1,41 @@
 import React from "react";
+import memesData from "..memesData.js";
+
 
 export default function Meme() {
+    const [memeImage, setMemeImage] = React.useState("");
+
+    function getMemeImage() {
+        const memesArray = memesData.data.memes;
+        const randomNumber = Math.floor(Math.random() * memesArray.length)
+        setMemeImage(memesArray[randomNumber].url);
+    }
+
     return (
-        <main>
-            <form className="form">
-                <div>
-                    <label htmlFor="top-text">Top Text</label>
-                    <input 
-                        id="top-text"
-                        type="text"
-                        placeholder="Shut up"
-                        className="form--input"
-                    />
-                </div>
-                <div>
-                    <label htmlFor="bottom-text">Bottom Text</label>
-                    <input
-                        id="bottom-text" 
-                        type="text"
-                        placeholder="and take my money"
-                        className="form--imput"
-                    />
-                </div>
-                <button className="form--button">Get a new meme image🖼️</button>
-            </form>
+        <main>  
+            <div className="form">
+                <label htmlFor="top-text">Top Text</label>
+                <input 
+                    id="top-text"
+                    type="text"
+                    placeholder="Shut up"
+                    className="form--input"
+                />
+                <label htmlFor="bottom-text">Bottom Text</label>
+                <input
+                    id="bottom-text" 
+                    type="text"
+                    placeholder="and take my money"
+                    className="form--imput"
+                />
+                <button 
+                    className="form--button"
+                    onclick={getMemeImage}
+                >
+                    Get a new meme image🖼️
+                </button>
+            </div>
+            <img scr={memeImage} className="meme--image" />
         </main>
     )
 }
