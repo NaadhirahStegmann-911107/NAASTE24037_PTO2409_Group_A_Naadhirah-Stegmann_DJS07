5d05d665f9ad0ae4d60d94f318deb565e8058ae6
Naadhirah Stegmann <naadhirahsteg@gmail.com>
2025-06-09 02:09:04 +0200

    APP not running.
    I've tried checking everything noted to see whether or not the app will render as it should but I'm just getting more and more errors

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,11 +1,9 @@
-import React from 'react';
-
 export default function Header() {
     return (
         <header className="header">
-            <img src="./images/Troll-Face.png" className="header--image"/>
+            <img src="./images/Troll-Face.png" className="header--image" alt="Troll Face"/>
             <h2 className="header--title">Header component</h2>
             <h4 className="header--project">React Course Project</h4>
         </header>
     )
-};
+}
